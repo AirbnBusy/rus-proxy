8aef734e6aa6e859787e589bb768f1daad6454f5
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-06-15 15:26:49 -0700

    Integrate rus-similar-listings and sam-booking

---
+++ sam-booking/database/config.js
@@ -0,0 +1,14 @@
+// LOCAL CONNECTION
+module.exports = {
+  database: 'bookings_module',
+  user: 'root',
+};
+
+// // DOCKER CONNECTION
+// module.exports = {
+//   host: 'db',
+//   port: 3306,
+//   database: 'bookings_module',
+//   user: 'user',
+//   password: 'user',
+// };
